333c0641aa54c61cbad69e671bbde98e9c06f33f
Harsh Mehta <mharsh13dec@gmail.com>
2021-07-28 16:34:30 +0530

    Theme setup!

--- lib/Core/Theme/Theme.dart
+++ lib/Core/Theme/Theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:rejolut_test_app/Core/Theme/constants.dart';
+import '../constants.dart';
 
 ThemeData buildThemeData() {
   return ThemeData(
